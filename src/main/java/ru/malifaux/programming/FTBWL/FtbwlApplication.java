1f8b4e55737e58e808a9f5d50c7dc38e585c0f7d
Vadim Danilov <bounce_man@live.com>
2019-02-05 23:37:55 +0300

    Initial commit

---
+++ src/main/java/ru/malifaux/programming/FTBWL/FtbwlApplication.java
@@ -0,0 +1,14 @@
+package ru.malifaux.programming.FTBWL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtbwlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FtbwlApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/ru/malifaux/programming/FTBWL/FtbwlApplicationTests.java
@@ -0,0 +1,17 @@
+package ru.malifaux.programming.FTBWL;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FtbwlApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
