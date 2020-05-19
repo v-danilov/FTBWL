f9432b575e2273ad5db1c981575c0ce97a371722
v-danilov <bounce_man@live.com>
2020-05-19 18:57:27 +0300

    Reworked EventCreateComponent

--- src/components/event/panel/EventCreateComponent.vue
+++ src/components/event/panel/EventCreateComponent.vue
@@ -1,42 +1,22 @@
 <template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
-  <v-dialog
-    v-model="dialog"
-    width="45%"
-  >
-    <v-stepper
-      v-model="e1"
-      alt-labels
-    >
+  <v-dialog v-model="dialog" width="45%">
+    <v-stepper v-model="e1" alt-labels>
       <v-stepper-header>
-        <v-stepper-step
-          :complete="e1 > 1"
-          step="1"
-        >Система</v-stepper-step>
+        <v-stepper-step :complete="e1 > 1" step="1">Система</v-stepper-step>
 
         <v-divider />
 
-        <v-stepper-step
-          :complete="e1 > 2"
-          step="2"
-        >Свойства</v-stepper-step>
+        <v-stepper-step :complete="e1 > 2" step="2">Свойства</v-stepper-step>
 
         <v-divider />
 
-        <v-stepper-step
-          :complete="e1 > 3"
-          step="3"
-        >Раунды</v-stepper-step>
-
+        <v-stepper-step :complete="e1 > 3" step="3">Раунды</v-stepper-step>
       </v-stepper-header>
 
       <v-stepper-items>
         <!-- Выбор системы-->
         <v-stepper-content step="1">
-          <v-row
-            class="fill-height"
-            align="center"
-            justify="center"
-          >
+          <v-row class="fill-height" align="center" justify="center">
             <v-col cols="6">
               <v-combobox
                 :items="gamesSystemsFromCache"
@@ -55,7 +35,6 @@
           >
             Продолжить
           </v-btn>
-
         </v-stepper-content>
 
         <!-- Информация о турнире-->
@@ -63,10 +42,7 @@
           <v-row justify="center">
             <!-- Название -->
             <v-col cols="8">
-              <v-text-field
-                v-model="eventName"
-                label="Название"
-              />
+              <v-text-field v-model="eventName" label="Название" />
             </v-col>
           </v-row>
           <v-row justify="center">
@@ -125,21 +101,14 @@
               />
             </v-col>
           </v-row>
-          <v-btn
-            @click="e1 = 3"
-            color="primary"
-          >
+          <v-btn @click="e1 = 3" color="primary">
             Продолжить
           </v-btn>
         </v-stepper-content>
 
         <v-stepper-content step="3">
           <!-- Заголовок -->
-          <v-row
-            align="center"
-            justify="center"
-            class="text-center"
-          >
+          <v-row align="center" justify="center" class="text-center">
             <v-col>
               <h3>Настройка раунда</h3>
             </v-col>
@@ -150,31 +119,26 @@
           <!-- Создание раундов -->
           <v-row>
             <v-col>
-              <v-row
-                dense
-                align="center"
-              >
+              <v-row align="center">
                 <!-- Дата игрового дня -->
                 <v-col cols="2">
                   <span>Дата</span>
                 </v-col>
-                <v-col>
+                <v-col cols="8">
                   <v-menu
                     v-model="menu"
                     :close-on-content-click="false"
-                    :return-value.sync="date"
                     ref="menu"
                     transition="scale-transition"
                     offset-y
-                    lazy
                     full-width
                     min-width="290px"
                   >
                     <template v-slot:activator="{ on }">
                       <v-text-field
-                        v-model="date"
+                        v-model="dateFormatted"
                         v-on="on"
-                        prepend-icon="event"
+                        append-icon="event"
                         readonly
                       />
                     </template>
@@ -185,55 +149,47 @@
                       color="secondary"
                     >
                       <v-spacer />
-                      <v-btn
-                        @click="menu = false"
-                        text
-                        rounded
-                        color="accent"
-                      >Отмена</v-btn>
+                      <v-btn @click="menu = false" text rounded color="accent"
+                        >Отмена</v-btn
+                      >
                       <v-btn
                         @click="$refs.menu.save(date)"
                         text
                         rounded
                         color="secondary"
-                      >OK</v-btn>
+                        >OK</v-btn
+                      >
                     </v-date-picker>
                   </v-menu>
                 </v-col>
               </v-row>
-              <v-row
-                dense
-                align="center"
-              >
-                <v-col>
+              <v-row align="center">
+                <v-col cols="2">
                   <span>Время</span>
                 </v-col>
-                <v-col>
+                <v-col cols="3">
                   <v-text-field
-                    v-model="timeStart"
+                    v-model="roundTimeStart"
                     v-mask="timeMask"
                     single-line
-                    label="Начало"
+                    label="12:11"
                   />
                 </v-col>
-                <v-col>
+                <v-col cols="3">
                   <v-text-field
-                    v-model="timeEnd"
+                    v-model="roundTimeEnd"
                     v-mask="timeMask"
                     single-line
-                    label="Конец"
+                    label="13:10"
                   />
                 </v-col>
-                <v-col cols="1">
+                <v-col cols="2">
                   <v-btn
-                    :disabled="saveIsActive"
+                    :disabled="!timeIsConsistent"
                     @click="saveRoundTime"
-                    icon
-                    fab
-                    text
                     rounded
+                    icon
                     color="primary"
-                    class="mx-1"
                   >
                     <v-icon>
                       queue
@@ -243,24 +199,29 @@
               </v-row>
             </v-col>
             <v-col>
-              <v-row
-                v-for="(day, index) in this.roundsDates"
-                :key="index"
-                dense
-              >
-                <v-col>
-                  {{ formatDate(day) }}
-                  <v-btn
-                    @click="deleteDay(index)"
-                    small
-                    color="accent"
-                    icon
-                    text
-                  >
-                    <v-icon>delete_outline</v-icon>
-                  </v-btn>
-                </v-col>
-              </v-row>
+              <v-list two-line>
+                <v-list-item
+                  v-for="(rounDate, index) in this.roundsDates"
+                  :key="index"
+                >
+                  <v-list-item-content>
+                    <v-list-item-title
+                      v-text="
+                        roundTimeBoundToString(rounDate.start, rounDate.end)
+                      "
+                    ></v-list-item-title>
+                    <v-list-item-subtitle
+                      v-text="rounDate.end.toDateString()"
+                    ></v-list-item-subtitle>
+                  </v-list-item-content>
+
+                  <v-list-item-action>
+                    <v-btn icon text color="accent" @click="deleteRound(index)">
+                      <v-icon>delete_outline</v-icon>
+                    </v-btn>
+                  </v-list-item-action>
+                </v-list-item>
+              </v-list>
             </v-col>
           </v-row>
 
@@ -271,14 +232,12 @@
           >
             Создать турнир
           </v-btn>
-
         </v-stepper-content>
       </v-stepper-items>
     </v-stepper>
   </v-dialog>
 </template>
 <script>
-
 import { END_POINTS } from '../../util/constants/EndPointsConstants'
 import { mask, tokens } from 'vue-the-mask'
 import { NOTIFICATION_TYPES } from '@/components/notifications/notificationTypes'
@@ -287,7 +246,8 @@ import UserCookiesClass from '../../../store/cookie/UserCookiesClass'
 export default {
   name: 'EventCreateComponent',
   directives: {
-    mask, tokens
+    mask,
+    tokens
   },
   props: {
     visible: {
@@ -295,23 +255,28 @@ export default {
       required: true
     }
   },
-  data () {
-    return {
-      e1: 0,
-      menu: false,
-      eventName: '',
-      selectedSystem: null,
-      selectedFormat: null,
-      selectedPlace: null,
-      selectedRulePack: null,
-      price: 0,
-      date: new Date().toISOString().substr(0, 10),
-      timeMask: 'Hh:Mm', // ^(0[0-9]|1[0-9]|2[0-3]|[0-9]):[0-5][0-9]$
-      timeStart: '',
-      timeEnd: '',
-      roundsDates: []
-    }
+  created () {
+    tokens.H = { pattern: /[0-2]/ }
+    tokens.h = { pattern: /[0-9]/ }
+    tokens.M = { pattern: /[0-5]/ }
+    tokens.m = { pattern: /[0-9]/ }
   },
+  data: vm => ({
+    e1: 0,
+    menu: false,
+    eventName: '',
+    selectedSystem: null,
+    selectedFormat: null,
+    selectedPlace: null,
+    selectedRulePack: null,
+    price: 0,
+    date: new Date().toISOString().substr(0, 10),
+    timeMask: 'Hh:Mm', // ^(0[0-9]|1[0-9]|2[0-3]|[0-9]):[0-5][0-9]$
+    roundTimeStart: '',
+    roundTimeEnd: '',
+    roundsDates: [],
+    timeIsConsistent: false
+  }),
   computed: {
     dialog: {
       get () {
@@ -330,76 +295,109 @@ export default {
       return Array.from(this.$store.getters.cachedFormats.values())
     },
     rulePacksFromCache () {
-      console.log(this.$store.getters)
       return Array.from(this.$store.getters.cachedRulePacks.values())
     },
     placesFromCache () {
       return this.$store.getters.cachedPlaces
     },
-    saveIsActive () {
-      const startSplitted = this.parseTime(this.timeStart)
-      const endSplitted = this.parseTime(this.timeEnd)
-      return (Number(startSplitted.hours + startSplitted.minutes) >= Number(endSplitted.hours + endSplitted.minutes)) // revert because of 'disabled' property
-    },
     organizerName () {
       let userOrganizer = UserCookiesClass.getAutheticatedUser()
-      return userOrganizer.firstName + ' \'' + userOrganizer.nickname + '\' ' + userOrganizer.lastName
+      return (
+        userOrganizer.firstName +
+        " '" +
+        userOrganizer.nickname +
+        "' " +
+        userOrganizer.lastName
+      )
+    },
+    dateFormatted () {
+      return this.formatDate(this.date)
+    },
+    roundStartDate () {
+      if (this.roundTimeStart) {
+        return this.setTimeToSelctedDate(this.roundTimeStart)
+      }
+      return null
+    },
+    roundEndDate () {
+      if (this.roundTimeEnd) {
+        return this.setTimeToSelctedDate(this.roundTimeEnd)
+      }
+      return null
     }
   },
-  created () {
-    tokens.H = { pattern: /[0-2]/ }
-    tokens.h = { pattern: /[0-9]/ }
-    tokens.M = { pattern: /[0-5]/ }
-    tokens.m = { pattern: /[0-9]/ }
+  watch: {
+    // using roundStartDate & roundEndDate because computed cache not reactive Date object
+    roundStartDate (newValue, oldValue) {
+      this.checkRoundDates(newValue, this.roundEndDate)
+    },
+    roundEndDate (newValue, oldValue) {
+      this.checkRoundDates(this.roundStartDate, newValue)
+    }
   },
   methods: {
+    formatDate (date) {
+      if (!date) return null
+
+      const [year, month, day] = date.split('-')
+      return `${day}.${month}.${year}`
+    },
     saveRoundTime () {
-      const startSplitted = this.parseTime(this.timeStart)
-      const endSplitted = this.parseTime(this.timeEnd)
       // whats going on? we have kek risk
       // https://stackoverflow.com/questions/5619202/converting-a-string-to-a-date-in-javascript
-      const parts = this.date.split('-')
-      const startDate = new Date(parts[0], parts[1] - 1, parts[2]).setHours(startSplitted.hours, startSplitted.minutes, 0)
-      const endDate = new Date(parts[0], parts[1] - 1, parts[2]).setHours(endSplitted.hours, endSplitted.minutes, 0)
-      const roundDate = { timeStart: startDate, timeEnd: endDate }
-      if (this.thereIsNoCrossDates(roundDate)) {
-        roundDate.timeStart = new Date(roundDate.timeStart).toISOString()
-        roundDate.timeEnd = new Date(roundDate.timeEnd).toISOString()
-        this.roundsDates.push(roundDate)
-        this.timeStart = ''
-        this.timeEnd = ''
+      const roundDates = { start: this.roundStartDate, end: this.roundEndDate }
+      if (this.thereIsNoCrossDates(roundDates)) {
+        roundDates.start = new Date(roundDates.start)
+        roundDates.end = new Date(roundDates.end)
+        this.roundsDates.push(roundDates)
+        this.roundTimeStart = ''
+        this.roundTimeEnd = ''
       }
     },
-    deleteDay (index) {
+    deleteRound (index) {
       this.roundsDates.splice(index, 1)
     },
     thereIsNoCrossDates (dateToCheck) {
       for (const val of this.roundsDates) {
-        if (!((dateToCheck.timeStart < val.timeStart &&
-            dateToCheck.timeEnd < val.timeStart) ||
-            dateToCheck.timeStart > val.timeEnd)) {
+        if (
+          !(
+            (dateToCheck.start < val.end && dateToCheck.end < val.start) ||
+            dateToCheck.start > val.end
+          )
+        ) {
           return false
         }
       }
       return true
     },
     createEvent () {
-      const event = [{
-        name: this.eventName,
-        place: { id: this.selectedPlace.id },
-        gameSystem: { code: this.selectedSystem.code },
-        format: { code: this.selectedFormat.code },
-        rulepack: { code: this.selectedRulePack.code },
-        price: this.price,
-        rounds: this.roundsDates
-      }]
+      const event = [
+        {
+          name: this.eventName,
+          place: { id: this.selectedPlace.id },
+          gameSystem: { code: this.selectedSystem.code },
+          format: { code: this.selectedFormat.code },
+          rulepack: { code: this.selectedRulePack.code },
+          price: this.price,
+          rounds: this.roundsDates
+        }
+      ]
       console.log(event)
-      this.$http.post(END_POINTS.EVENTS.DEFAULT, event).then(response => {
-        this.$store.dispatch('notifications/add', {type: NOTIFICATION_TYPES.SUCCESS, text: 'Event created <link>'})
-      }).catch((err) => {
-        this.$store.dispatch('notifications/add', {type: NOTIFICATION_TYPES.ERROR, text: 'Event creation error. Try again later'})
-        console.log(err)
-      })
+      this.$http
+        .post(END_POINTS.EVENTS.DEFAULT, event)
+        .then(response => {
+          this.$store.dispatch('notifications/add', {
+            type: NOTIFICATION_TYPES.SUCCESS,
+            text: 'Event created <link>'
+          })
+        })
+        .catch(err => {
+          this.$store.dispatch('notifications/add', {
+            type: NOTIFICATION_TYPES.ERROR,
+            text: 'Event creation error. Try again later'
+          })
+          console.log(err)
+        })
     },
     calculateEventDaysByRounds () {
       let firstDate, lastDate
@@ -415,16 +413,29 @@ export default {
       }
       return { firstDate: firstDate, lastDate: lastDate }
     },
-    parseTime (val) {
-      const splitted = val.split(':')
-      return { hours: splitted[0], minutes: splitted[1] }
+    setTimeToSelctedDate (time) {
+      const splitted = time.split(':')
+      let dateWithTime = new Date(this.date)
+      dateWithTime.setHours(splitted[0])
+      dateWithTime.setMinutes(splitted[1])
+      return dateWithTime
     },
-    formatDate (date) {
-      const sDate = new Date(date.timeStart)
-      const eDate = new Date(date.timeEnd)
-      return sDate.getDate() + '' + '.' + (sDate.getMonth() + 1) + '.' + sDate.getFullYear() +
-          ' (' + sDate.getHours() + ':' + sDate.getMinutes() + ' - ' + eDate.getHours() + ':' + eDate.getMinutes() + ')'
+    checkRoundDates (startDate, endDate) {
+      if (!this.dateIsValid(startDate) || !this.dateIsValid(endDate)) {
+        this.timeIsConsistent = false
+      }
+      this.timeIsConsistent = startDate < endDate
+    },
+    dateIsValid (d) {
+      return d instanceof Date && !isNaN(d)
+    },
+    roundTimeBoundToString (startDate, endDate) {
+      const start = startDate.getHours() + ':' + startDate.getMinutes()
+      const end = endDate.getHours() + ':' + endDate.getMinutes()
+      return start + ' - ' + end
     }
   }
 }
 </script>
+
+<style scoped></style>
